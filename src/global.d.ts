6a6c8648b2d1d63951f65f0b9ef9d4ad2f83e454
Farewe1ll <me@Farewe1ll.top>
2025-12-12 21:38:20 +0800

    fixed npm check

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,6 +4,8 @@ declare global {
 	interface Window {
 		// type from '@swup/astro' is incorrect
 		swup: AstroIntegration;
+		// Sidebar padding adjustment helper
+		adjustPageForSidebar?: () => void;
 		pagefind: {
 			search: (query: string) => Promise<{
 				results: Array<{
